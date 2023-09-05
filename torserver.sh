b2c79c2d1f54068d892a3431a181a1024da80702
LilyAsteria <128959146+LilyAsteria@users.noreply.github.com>
2023-09-06 05:41:07 +0700

    Create torserver.sh

---
+++ torserver.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Step 1: Install required packages
+sudo apt-get update -y
+sudo apt-get install unattended-upgrades apt-listchanges apt-transport-https -y
+
+# Step 2: Edit configuration files
+# Create /etc/apt/apt.conf.d/50unattended-upgrades
+echo 'Unattended-Upgrade::Origins-Pattern {
+    "origin=Debian,codename=${distro_codename},label=Debian-Security";
+    "origin=TorProject";
+};
+Unattended-Upgrade::Package-Blacklist {
+};' | sudo tee /etc/apt/apt.conf.d/50unattended-upgrades > /dev/null
+
+# Update /etc/apt/apt.conf.d/20auto-upgrades
+echo 'APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::AutocleanInterval "5";
+APT::Periodic::Unattended-Upgrade "1";
+APT::Periodic::Verbose "1";' | sudo tee /etc/apt/apt.conf.d/20auto-upgrades > /dev/null
+
+# Step 3: Automatically reboot
+echo 'Unattended-Upgrade::Automatic-Reboot "true";' | sudo tee -a /etc/apt/apt.conf.d/50unattended-upgrades > /dev/null
+
+# Step 4: Configure Tor
+# Create /etc/apt/sources.list.d/tor.list
+echo 'deb     [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org bullseye main
+deb-src [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org bullseye main' | sudo tee /etc/apt/sources.list.d/tor.list > /dev/null
+
+# Import Tor GPG key
+sudo wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | sudo gpg --dearmor | sudo tee /usr/share/keyrings/tor-archive-keyring.gpg >/dev/null
+
+# Update package lists
+sudo apt-get update -y
+
+# Install Tor and Nyx
+sudo apt-get install tor deb.torproject.org-keyring nyx -y
+
+# Configure Tor
+echo 'Nickname    Left4Dead2  # Change "myNiceRelay" to something you like
+# Write your e-mail and be aware it will be published
+ORPort      443          # You might use a different port, should you want to
+ExitRelay   0
+SocksPort   0
+ControlPort 6942' | sudo tee /etc/tor/torrc > /dev/null
+
+# Step 5: Restart the Tor service
+sudo systemctl restart tor@default
+sudo systemctl restart tor
